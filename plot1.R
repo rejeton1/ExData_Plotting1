5cb62a1c21492d4ba431b63fffaa0a14c8bbd240
sangwoo <138969571+rejeton1@users.noreply.github.com>
2023-08-30 20:46:07 +0900

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,9 +1,18 @@
-library(dplyr)
+##load needed packages
 library(stats)
 library(base)
+library(dplyr)
+
+## read entire data and extract only needed data
 data <- read.csv('household_power_consumption.txt', sep = ';')
 needed_data <- filter(data, (Date == '1/2/2007'|Date =='2/2/2007'))
-head(needed_data)
 
-needed_data[,1] <- as.Date(needed_data[,1], '%d/%m/%Y')
-# needed_data[,2] <- strptime(needed_data[,2], '%H:%M:%S')
+## convert character into numeric
+needed_data$Global_active_power <- as.numeric(needed_data$Global_active_power)
+
+##open png graphic device and plotting graph(plot1)
+png(filename='plot1.png', width = 480, height = 480)
+hist(needed_data$Global_active_power, col = 'red', 
+     xlab = 'Global Active Power (kilowatts)', 
+     main = 'Global Active Power')
+dev.off()
